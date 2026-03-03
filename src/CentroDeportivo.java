73e448cd44c5a15d7a067689ce6c846191fa8248
Alejandro Muriel <amuripra@myuax.com>
2026-03-03 09:39:17 +0100

    feat (Main) creado metodos buscarEntrenador y buscarPrimerHuecoLibre

--- src/CentroDeportivo.java
+++ src/CentroDeportivo.java
@@ -1,3 +1,66 @@
 public class CentroDeportivo {
-    private final Entrenador[15] ENTRENADORES;
+
+    //Atributos
+    private Entrenador[] entrenadores;
+    private final int NUM_MAX_ENTRENADORES;
+
+    public CentroDeportivo(Entrenador[] entrenadores) {
+        this.NUM_MAX_ENTRENADORES = 15;
+
+        this.entrenadores = new Entrenador[NUM_MAX_ENTRENADORES];
+    }
+
+    /**
+     *
+     * @param id
+     * @return
+     */
+    public Entrenador buscarEntrenador(int id){
+        boolean encontrado = false;
+        Entrenador entrenadorBuscado = null;
+        int i = 0;
+
+        while (!encontrado && i < NUM_MAX_ENTRENADORES) {
+            if (entrenadores[i] != null && entrenadores[i].getId() == id) {
+                entrenadorBuscado = entrenadores[i];
+                encontrado = true;
+            }
+
+            i++;
+        }
+
+        return entrenadorBuscado;
+    }
+
+    /**
+     *
+     * @return
+     */
+    private int buscarPrimerHuecoLibre(){
+        boolean encontrado = false;
+        int primerHuecoLibre = -1;
+        int i = 0;
+
+        while (!encontrado && i < NUM_MAX_ENTRENADORES) {
+            if (entrenadores[i] == null){
+                primerHuecoLibre = i;
+                encontrado = true;
+            }
+
+            i++;
+        }
+
+        return primerHuecoLibre;
+    }
+
+    /**
+     *
+     * @param entrenador
+     * @return
+     */
+    public boolean registrarEntrenador(Entrenador entrenador){
+
+    }
+    public String mostrarEntrenadores()
+    public int contarEntrenadores()
 }
